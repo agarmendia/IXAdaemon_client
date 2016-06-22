96acb4228cb340e2a200b6ac89ce0121771239b1
agarmendia <aitzolgarmendia@gmail.com>
2016-06-23 01:20:31 +0200

    IXAdaemon_client updated and corrected

--- main.go
+++ main.go
@@ -6,26 +6,27 @@ import (
 	"fmt"
 	"net"
 	"os"
-	//"time"
 )
 
 func main() {
 
 	var port string
+	var strn string
 
 	//Create channels for sincronizing with go rutines
-	c := make(chan struct{})
-	ch := make(chan struct{})
+	c := make(chan bool, 1)
+	ch := make(chan bool, 1)
 
 	//Parse Port number, for communicating with IXAdaemon_server
 	flag.StringVar(&port, "port", "2101", "mainPort of server")
 	flag.Parse()
 
 	//Connect with IXAdaemon_server
-	mainService := ":" + port
-	tcpAddr1, err := net.ResolveTCPAddr("tcp", mainService)
-	conn, err := net.DialTCP("tcp", nil, tcpAddr1)
+	//mainService := ":" + port
+	//tcpAddr1, err := net.ResolveTCPAddr("tcp4", mainService)
+	conn, err := net.Dial("tcp", "127.0.0.1:"+port)
 	if err != nil {
+		fmt.Println(err)
 		fmt.Println("Can't connect to server")
 		os.Exit(1)
 	}
@@ -38,53 +39,61 @@ func main() {
 	interceptSignals(conn)
 
 	//Launch routines
-	go write_server(*conn, *bufin, c)
+	go write_server(conn, *bufin, c, &strn)
 	go read_server(*sc, *bufout, ch)
 	//write_server ended
-	<-c
+	correctWriting := <-c
+	if correctWriting == false {
+		//fmt.Println("Failure Writing Server")
+	}
 	//read_server ended
-	<-ch
+	correctReading := <-ch
+	if correctReading == false {
+		//fmt.Println("Failure Reading Server")
+	}
 
 }
 
-func write_server(conn net.TCPConn, bufin bufio.Writer, c chan struct{}) {
+func write_server(conn net.Conn, bufin bufio.Writer, c chan bool, strn *string) {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Split(bufio.ScanLines)
 	for scanner.Scan() {
 		str := scanner.Text()
+		*strn = *strn + str + "\n"
 		_, err := bufin.WriteString(string(str))
 		if err != nil {
-			fmt.Print("1")
-			fmt.Println(err)
+			c <- true
+			return
 		}
 		_, err = bufin.WriteString("\n")
 		if err != nil {
-			fmt.Print("2")
-			fmt.Println(err)
+			c <- true
+			return
 		}
 		err = bufin.Flush()
 		if err != nil {
-			fmt.Print("3")
-			fmt.Println(err)
+			c <- true
+			return
 		}
 	}
 	bufin.WriteString("[IXAdaemon]EOF\n")
 	bufin.Flush()
-	c <- struct{}{}
+	c <- false
 	return
 }
 
-func read_server(scanner bufio.Scanner, bufout bufio.Reader, c chan struct{}) {
+func read_server(scanner bufio.Scanner, bufout bufio.Reader, c chan bool) {
 	//canner := bufio.NewScanner(conn)
 	scanner.Split(bufio.ScanLines)
 	for scanner.Scan() {
 		str := scanner.Text()
 		if str == "[IXAdaemon]EOD" {
-			break
+			c <- true
+		} else {
+			//bufio.NewWriter(os.Stdout).WriteString(str)
+			fmt.Println(str)
 		}
-		//bufio.NewWriter(os.Stdout).WriteString(str)
-		fmt.Println(str)
 	}
-	c <- struct{}{}
+	c <- true
 	return
 }
